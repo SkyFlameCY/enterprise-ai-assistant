03a0b8106f970103ab3c271e7b556d4c4b7c8728
sanwei <1959970441@qq.com>
2026-09-17 10:37:58 +0800

    feat: 完成 PostgreSQL + pgvector 基础设施与数据库连通性

--- src/test/java/cn/coder/sanwei/enterpriseaiassistant/EnterpriseAiAssistantApplicationTests.java
+++ src/test/java/cn/coder/sanwei/enterpriseaiassistant/EnterpriseAiAssistantApplicationTests.java
@@ -2,7 +2,9 @@ package cn.coder.sanwei.enterpriseaiassistant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class EnterpriseAiAssistantApplicationTests {
 
